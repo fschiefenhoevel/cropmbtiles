7f9263ed9175b96eb9847aecdde24abffbe935a3
Fridtjof Schiefenhövel <fridtjof.schiefenhoevel@tum.de>
2023-11-13 23:09:38 +0100

    added automated info extraction via gdalinfo

--- cropmbtiles.sh
+++ cropmbtiles.sh
@@ -4,40 +4,61 @@
 ## variables needed to be set: ###
 ##################################
 
-# Filenames
+# input filenames
 mbtile_original=/media/fridtjof/ELEMENTS/skitourenguru/originals/AU_SG_TOPO15_MINI4.1.mbtiles
-polygon=/media/fridtjof/ELEMENTS/skitourenguru/originals/polygone/Suedtirol.shp
+polygon=/media/fridtjof/ELEMENTS/skitourenguru/originals/polygone/Osttirol.shp
+
+# desired maximum and minimum zoomlevel of output
+maxZoom=13
+minZoom=9
+
+##################################
+### variables that can be set ####
+#################################
+
+# possible formats = PNG, PNG8, JPEG
+#  n.b.: PNG uses ZLEVEL_1-9, which has nothing to do with zoomlevel. JPEG uses QUALITY=1-100, PNG8 uses DITHER=[YES/NO]
+# also, JPEG is smaller, yet doesn't have transparency
+# NO need to uncomment unnecessary options, i.e. you can leave ZLEVEL set to 6 and still use PNG8, which doesn't use it.
+format=PNG8
+ZLEVEL=6 # default=6
+QUALITY=75 # default=75
+DITHER=NO # default=NO
 
 # native max zoomlevel of mbtiles file
-nativeMaxZoom=15
+# done automatically with gdalinfo
+ginfo=$(gdalinfo $mbtile_original)
+nativeMaxZoom=$(echo $ginfo | sed -n "s/^.*maxzoom=\(\S*\).*$/\1/p")
 
-# desired max zoomlevel and gdal_translate ZLEVEL (if TILE_FORMAT=PNG is used). Please note: ZLEVEL has nothing to do with zoom-level!
-maxZoom=15
-minZoom=9
-format=PNG8
-ZLEVEL=6
+# uncomment to set manually
+# nativeMaxZoom=16
+
+
+
+# maxímum resolution of input:
+# get maximum resolution automatically from gdalinfo
+maxRes=$(echo $ginfo | sed -n "s/^.*Pixel Size = (\(\S*\),.*$/\1/p")
+
+# alternatively, this can be calculated with formula:
+# according to: https://wiki.openstreetmap.org/wiki/Zoom_levels 
+# formula (easily readable):
+#(circ*cos(0))/2^(0+8)
+circ=40075017
+term1=$(echo 2^$(echo $nativeMaxZoom+8 | bc) | bc)
+# uncomment the following line to calculate maxiumum resolution by formula 
+#maxRes=$(echo $circ/$term1 | bc -l)
+
+
 
 ###################################
 ##### no changes from here on #####
 ###################################
 
-# calculate pixel-per-meter resolution (at equator?) for zoom levels, needed for gdal_translate (?)
-# according to: https://wiki.openstreetmap.org/wiki/Zoom_levels 
-# (approx.) circumreference of earth in meters
-
-circ=40075017
-
-# formula (easily readable):
-#(circ*cos(0))/2^(0+8)
-
-term1=$(echo 2^$(echo $nativeMaxZoom+8 | bc) | bc)
-calcMaxRes=$(echo $circ/$term1 | bc -l)
-
-echo 'native maximum resolution:' $calcMaxRes
+echo 'native maximum resolution:' $maxRes
 
 diffZoom=$(echo $nativeMaxZoom-$maxZoom | bc)
 mult=$(echo 2^$diffZoom | bc)
-ans=$(echo $calcMaxRes*$mult | bc)
+ans=$(echo $maxRes*$mult | bc)
 
 steps=$(echo $maxZoom-$minZoom | bc)
 
@@ -48,14 +69,14 @@ if [[ $nativeMaxZoom == $maxZoom ]] ;then
 	
 	echo "NO change in maximum zoom level!"
 	gdalwarp -overwrite -of vrt -cutline $polygon -crop_to_cutline $mbtile_original lvl$maxZoom.vrt
-	gdal_translate -of mbtiles -co "TILE_FORMAT=$format" -co "ZLEVEL="$ZLEVEL"" "lvl"$maxZoom".vrt" "map.mbtiles"
+	gdal_translate -of mbtiles -co "TILE_FORMAT=$format" -co "ZLEVEL="$ZLEVEL"" -co "QUALITY="$QUALITY"" -co "DITHER="$DITHER""  "lvl"$maxZoom".vrt" "map.mbtiles"
 	echo '"lvl'$(echo $maxZoom-$n | bc)'.vrt"'
 	
 	while [[ $n -lt $steps  ]]
         do
         ans=$(echo $ans *2 | bc)
         gdalwarp -overwrite -ovr $n -tr $ans $ans -of vrt -cutline $polygon -crop_to_cutline $mbtile_original lvl$(echo $maxZoom -$n-1 | bc).vrt
-        gdal_translate -of mbtiles -co "TILE_FORMAT=$format" -co "ZLEVEL="$ZLEVEL"" "lvl"$(echo $maxZoom-$n-1| bc)".vrt" "lvl"$(echo $maxZoom-$n-1| bc)".mbtiles"
+        gdal_translate -of mbtiles -co "TILE_FORMAT=$format" -co "ZLEVEL="$ZLEVEL"" -co "QUALITY="$QUALITY"" -co "DITHER="$DITHER"" "lvl"$(echo $maxZoom-$n-1| bc)".vrt" "lvl"$(echo $maxZoom-$n-1| bc)".mbtiles"
         echo '"lvl'$(echo $maxZoom-$n-1| bc)'.vrt"'
         (( n++ ))
 	done
@@ -65,7 +86,7 @@ else
         echo "Change in maximum zoom level!"
 	echo 'Highest resolution in new file:' $ans
 	gdalwarp -overwrite -ovr $ovr -tr $ans $ans -of vrt -cutline $polygon -crop_to_cutline $mbtile_original lvl$maxZoom.vrt	
-	gdal_translate -of mbtiles -co "TILE_FORMAT=$format" -co "ZLEVEL="$ZLEVEL"" "lvl"$maxZoom".vrt" "map.mbtiles"	
+	gdal_translate -of mbtiles -co "TILE_FORMAT=$format" -co "ZLEVEL="$ZLEVEL"" -co "QUALITY="$QUALITY"" -co "DITHER="$DITHER"" "lvl"$maxZoom".vrt" "map.mbtiles"	
 	
 	while [[ $n -lt $steps  ]]
         do
@@ -73,7 +94,7 @@ else
         (( n++ ))
 	ans=$(echo $ans *2 | bc)
         gdalwarp -overwrite -ovr $ovr -tr $ans $ans -of vrt -cutline $polygon -crop_to_cutline $mbtile_original lvl$(echo $maxZoom -$n | bc).vrt
-        gdal_translate -of mbtiles -co "TILE_FORMAT=$format" -co "ZLEVEL="$ZLEVEL"" "lvl"$(echo $maxZoom-$n | bc)".vrt" "lvl"$(echo $maxZoom-$n | bc)".mbtiles"
+        gdal_translate -of mbtiles -co "TILE_FORMAT=$format" -co "ZLEVEL="$ZLEVEL"" -co "QUALITY="$QUALITY"" -co "DITHER="$DITHER"" "lvl"$(echo $maxZoom-$n | bc)".vrt" "lvl"$(echo $maxZoom-$n | bc)".mbtiles"
         echo '"lvl'$(echo $maxZoom-$n | bc)'.vrt"'
         done
 
